eaadc81953eb15c9c8269032429681d66d864d21
sharopcha <napster1202@gmail.com>
2021-09-20 21:53:05 +0300

    grocery manager consumer added

--- lib/screens/grocery_screen.dart
+++ lib/screens/grocery_screen.dart
@@ -1,11 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipe_apprentice/models/models.dart';
 import 'package:food_recipe_apprentice/screens/empty_grocery_screen.dart';
+import 'package:provider/provider.dart';
 
 class GroceryScreen extends StatelessWidget {
   const GroceryScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return EmptyGroceryScreen();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        // TODO: Present GroceryItemScreen
+        onPressed: () {},
+      ),
+      body: buildGroceryScreen(),
+    );
+  }
+
+  Widget buildGroceryScreen() {
+    return Consumer<GroceryManager>(
+      builder: (context, manager, child) {
+        if (manager.groceryItem.isNotEmpty) {
+          // TODO: add GroceryListScreen
+          return Container();
+        } else {
+          return const EmptyGroceryScreen();
+        }
+      },
+    );
   }
 }

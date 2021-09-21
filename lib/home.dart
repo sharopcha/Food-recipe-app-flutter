7ad0f6d1bf0950c8542b3142588f21c78acaf883
sharopcha <napster1202@gmail.com>
2021-09-22 01:32:26 +0300

    fixed cache problem/preserving a state of the tabs

--- lib/home.dart
+++ lib/home.dart
@@ -31,7 +31,10 @@ class _HomeState extends State<Home> {
               style: Theme.of(context).textTheme.headline6,
             ),
           ),
-          body: pages[tabManager.selectedTab],
+          body: IndexedStack(
+            index: tabManager.selectedTab,
+            children: pages,
+          ),
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: tabManager.selectedTab,
             onTap: (index) {

1345f87134a2372acdd67336a9cefcd22d00b272
sharopcha <napster1202@gmail.com>
2021-09-22 12:42:28 +0300

    app state manager setup

---
+++ lib/models/app_state_manager.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class FooderlichTab {
+  static const int explore = 0;
+  static const int recipes = 1;
+  static const toBuy = 2;
+}
+
+class AppStateManager extends ChangeNotifier {
+  bool _initialized = false;
+  bool _loggedIn = false;
+  bool _onboardingComplete = false;
+  int _selectedTab = FooderlichTab.explore;
+
+  bool get isInitialized => _initialized;
+  bool get isLoggedIn => _loggedIn;
+  bool get isOnboardingComplete => _onboardingComplete;
+  int get getSelectedTab => _selectedTab;
+
+  // TODO: add initialized app
+  // TODO: add login
+  // TODO: add completeOnboarding
+  // TODO: add goToTab
+  // TODO: add goToRecipes
+  // TODO: add logout
+}
